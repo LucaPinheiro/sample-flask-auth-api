661d49edaaccf2d4421709a0860fd71a8edd281d
LucaPinheiro <lucapgomes11@gmail.com>
2024-05-05 23:51:55 -0300

    feat: Add login functionality to app.py

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask
 from models.user import User
 from database import db
+from flask_login import LoginManager
+from flask import request
+from flask import jsonify
 
 app = Flask(__name__)
 app.config['SECRETY_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+login_manager = LoginManager() 
+
 db.init_app(app)
+login_manager.init_app(app)
+
+@app.rout('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+    
+    if username and password:
+        pass
+
+    return jsonify({'message': 'Invalid credentials'}), 400
 
 @app.route('/hello-world', methods=['GET'])
 def hello_world():
