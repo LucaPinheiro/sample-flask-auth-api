8312cdc703382112722cf089a91126e2cc234ca5
LucaPinheiro <lucapgomes11@gmail.com>
2024-05-06 00:19:38 -0300

    feat: Implement login functionality and authentication in app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from models.user import User
 from database import db
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user
 from flask import request
 from flask import jsonify
 
@@ -13,7 +13,11 @@ login_manager = LoginManager()
 
 db.init_app(app)
 login_manager.init_app(app)
+login_manager.login_view = 'login'
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -24,6 +28,8 @@ def login():
     if username and password:
         user = User.query.filter_by(username=username).first()
         if user and user.check_password(password):
+            login_user(user)
+            print(current_user.is_authenticated)
             return jsonify({'message': 'Autenticação realizada com sucesso'}), 201
         return jsonify({'message': 'Autenticação realizada com sucesso'}), 201
 
